3876d0083dc9ba67c95b6910601895d7deab73ea
Hello2006_debug <memugengano@davidson.edu>
2024-11-12 14:52:46 -0500

    Prototype

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,15 @@
+var map = L.map('map').setView([35.687160, -80.112373], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-/* javascript */
+var marker = L.marker([35.687160, -80.112373]).addTo(map);
+var popup = L.popup();
+
+function onMapClick(e) {
+    marker.setLatLng(e.latlng);
+    marker.bindPopup("Hello This is Davidson College").openPopup();
+}
+
+map.on('click', onMapClick);
